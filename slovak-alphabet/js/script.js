ed98123718859d2ceb4d2a43cfe2e721044bbd06
neclor <neclordev@gmail.com>
2026-02-10 17:29:46 +0100

    color

--- slovak-alphabet/js/script.js
+++ slovak-alphabet/js/script.js
@@ -3,13 +3,18 @@ const alphabeth = [
     "I", "Í", "J", "K", "L", "Ĺ", "Ľ", "M", "N", "Ň", "O", "Ó", "Ô", "P", "Q", "R", "Ŕ",
     "S", "Š", "T", "Ť", "U", "Ú", "V", "W", "X", "Y", "Ý", "Z", "Ž"
 ];
+const vowels = ["A","Á","Ä","E","É","I","Í","O","Ó","Ô","U","Ú","Y","Ý"];
 
 const grid = document.getElementById("lettersGrid");
 const player = document.getElementById("audioPlayer");
 
 alphabeth.forEach(letter => {
     const btn = document.createElement("button");
-    btn.className = "letter";
+    btn.classList.add("letter");
+
+    if (vowels.includes(letter.toUpperCase())) btn.classList.add("vowel");
+    else btn.classList.add("consonant");
+
     btn.textContent = letter;
 
     btn.addEventListener("click", () => {
