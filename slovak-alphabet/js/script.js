2eaf51bfa8bfd9f0e2d1cc58b75f7539ced88122
neclor <neclordev@gmail.com>
2026-02-10 16:09:26 +0100

    slovak alphabeth

---
+++ slovak-alphabet/js/script.js
@@ -0,0 +1,21 @@
+const alphabeth = [
+    "A", "Á", "Ä", "B", "C", "Č", "D", "Ď", "Dz", "Dž", "E", "É", "F", "G", "H", "Ch",
+    "I", "Í", "J", "K", "L", "Ľ", "M", "N", "Ň", "O", "Ó", "Ô", "P", "Q", "R", "Ŕ",
+    "S", "Š", "T", "Ť", "U", "Ú", "V", "W", "X", "Y", "Ý", "Z", "Ž"
+];
+
+const grid = document.getElementById("lettersGrid");
+const player = document.getElementById("audioPlayer");
+
+alphabeth.forEach(letter => {
+    const btn = document.createElement("button");
+    btn.className = "letter";
+    btn.textContent = letter;
+
+    btn.addEventListener("click", () => {
+    player.src = `sounds/${letter}.mp3`;
+    player.play();
+    });
+
+    grid.appendChild(btn);
+});
